141b5dbe8c0eda75d54276d3eb5fb424abf4310a
samithox <samuel.sp10@gmail.com>
2018-03-20 14:48:30 -0300

    Mi appde prueba, primer commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\s&m\desktop\samuel\ejercicios\programacion\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
